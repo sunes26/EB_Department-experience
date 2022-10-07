1ae6bf9dbb624ecd39baeef654e0506ad108cae6
sunes26 <85713817+sunes26@users.noreply.github.com>
2022-10-07 22:59:16 +0900

    Update result.js

--- javascript/result.js
+++ javascript/result.js
@@ -3,6 +3,8 @@ const numbtn = document.getElementsByClassName("num_btn");
 const input = document.getElementById("num_input");
 const clear = document.getElementsByClassName("num_btn")[9];
 const check = document.getElementsByClassName("num_btn")[11];
+const festival = document.getElementById("festivals");
+const restart_btn = document.getElementById("restart_btn");
 
      
 let num_input = document.getElementById("num_input");
@@ -35,6 +37,11 @@ check.addEventListener("click", function () {
     setTimeout(function() {
       document.getElementsByClassName("safe_door")[0].classList.add("open");
     }, 2000);
+    setTimeout(function() {
+      festival.style.opacity = 1;
+      festival.style.zIndex="100";
+      restart_btn.style.opacity = 1;
+    }, 2400);
     
    
   }
